334c9e86aa2e9ab3b80fa1396e325be7a52a6ad8
Gideon Ladiyo <alwaysfun0604@gmail.com>
2024-09-26 21:46:16 +0700

    tugas

--- pertemuan_2/main.dart
+++ pertemuan_2/main.dart
@@ -1,18 +1,39 @@
 void main() {
-  List<Map<String, dynamic>> mahasiswa = [
-    {
-      "nilai": [12, 20, 30],
-      "Status": "nelum menikah"
-    },
-    {
+  // 1. buatkan algoritma untuk
+  Map<String, dynamic> mahasiswa ={
+  // - menghitung rata2 dari ipk
+    "Nama": ["Gideon", "Wahyu", "Luthfi", "Fauzan", "Dhafin", "Zaki"],
+    "IPK": [4, 3.89, 3.95, 2.99, 3.92, 2.35],
+    "Status": ["belum menikah", "sudah menikah", "sudah menikah", "belum menikah", "belum menikah", "sudah menikah"]
+  };
+  double total_ipk = 0;
+  for (var values in mahasiswa["IPK"]){
+    total_ipk += values;
+  };
+  double avg_ipk = total_ipk/mahasiswa["IPK"].length;
+  print("Rata-rata IPK: $avg_ipk");
 
-    },
-    {
-
-    },
-    {
-      
-    }
-  ];
-
+  // - menjumlahan berapa banyak mahasiswa yang sudah nikah
+  int jumlahBelumMenikah = 0;
+  int jumlahSudahMenikah = 0;
+  List namaSudahMenikah = [];
+  List namaBelumMenikah = [];
+  for (int i = 0; i < mahasiswa["Status"].length; i++){
+    if (mahasiswa["Status"]![i] == "belum menikah"){
+      jumlahBelumMenikah += 1;
+      namaBelumMenikah.add(mahasiswa["Nama"]![i]);
+    } else if(mahasiswa["Status"]![i] == "sudah menikah"){
+      jumlahSudahMenikah += 1;
+      namaSudahMenikah.add(mahasiswa["Nama"]![i]);
+    } else{
+      print("Terdapat kesalahan");
+    };
+  };
+  String gabunganGabunganSudahMenikah = namaSudahMenikah.join(", ");
+  String gabunganGabunganBelumMenikah = namaBelumMenikah.join(", ");
+  print("Jumlah mahasiswa belum menikah: $jumlahBelumMenikah");
+  print("Daftar nama mahasiswa belum menikah: $gabunganGabunganBelumMenikah");
+  print("Jumlah mahasiswa sudah menikah: $jumlahSudahMenikah");
+  print("Daftar nama mahasiswa belum menikah: $gabunganGabunganSudahMenikah");
+  // 2. masing2 dari point2 1a dan 1b diimplementasikan dalam kodingan
 }
